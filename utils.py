3ed70acd156bc04c4d0ed7d1d997ab6ebd577660
Bernat Martinez <bolicubo@gmail.com>
2020-05-13 03:29:34 +0200

    xd

--- utils.py
+++ utils.py
@@ -53,6 +53,7 @@ def plot_comparison(x1, y1, x2, y2, alg1, alg2, dataset):
     axs[0].scatter(x1[:, 0], x1[:, 1], c=y1)
     axs[1].set_title(alg2)
     axs[1].scatter(x2[:, 0], x2[:, 1], c=y2)
+    plt.tight_layout()
     plt.savefig(join(plot_dir, '{}_comp.png'.format(dataset)))
 
 
